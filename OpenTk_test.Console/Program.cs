fa48dfec21b973c9eab95732d90279a02ab62ad2
vovatrykoz <vovatrykoz@gmail.com>
2024-12-08 15:00:50 +0100

    minor change

--- OpenTk_test.Console/Program.cs
+++ OpenTk_test.Console/Program.cs
@@ -2,6 +2,8 @@
 
 Animation<OpenTK.Mathematics.Vector2> animation = new(OpenTK.Mathematics.Vector2.One, OpenTK.Mathematics.Vector2.One);
 
+animation.Update();
+
 class Animation<T>(T value, T update) where T : IAdditionOperators<T, T, T>
 {
     public T Update()
